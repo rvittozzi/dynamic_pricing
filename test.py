374560dd43d06a22f8ae2c4771afbc815ccb0f97
rvittozzi <137367134+rvittozzi@users.noreply.github.com>
2023-10-02 10:55:24 -0400

    added logic

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import requests
 import json
 from datetime import datetime, timedelta
+import math
 
 app = Flask(__name__)
 
@@ -88,7 +89,6 @@ def dynamic_pricing(base_rate, pricing_rules, stay_date, num_nights):
     return calculated_price
 
 
-
 def fetch_pricing_rules(uid):
     url = f"https://api.hostfully.com/v2/pricingrules/{uid}"
     response = requests.get(url, headers=headers)
@@ -118,6 +118,29 @@ def fetch_pricing_rules_keys(uid):
                     print(key)
     else:
         print(f"Failed to fetch pricing rules. Status code: {response.status_code}")
+
+
+def apply_weekend_rate(daily_rate, day, weekend_increase_percent, min_weekend_nights):
+    if day.weekday() >= 5:  # 5 = Saturday, 6 = Sunday
+        if min_weekend_nights <= 1:  # If it's not mandatory to stay for more than one night during weekends
+            return daily_rate * (1 + weekend_increase_percent / 100)
+    return daily_rate
+
+
+def apply_last_minute_discount(daily_rate, day, today, discount_days, discount_percents):
+    for days, percent in zip(discount_days, discount_percents):
+        if today + timedelta(days=days) >= day:
+            return daily_rate * (1 - percent / 100)
+    return daily_rate
+
+
+def apply_seasonal_rate(daily_rate, day, seasonal_rates):
+    for season in seasonal_rates:
+        start, end, percent, min_nights = season['start'], season['end'], season['percent'], season['min_nights']
+        if start <= day <= end:
+            if min_nights <= 1:  # If there is no minimum night requirement for the season
+                return daily_rate * (1 + percent / 100)
+    return daily_rate
 
 
 @app.route('/')
@@ -161,18 +184,35 @@ def show_price():
 
     for i in range(num_nights):
         day = from_date_obj + timedelta(days=i)
-        daily_rate = fetch_price_for_date(property_uid, day.strftime('%Y-%m-%d'))
 
+        # Ensure daily_rate is defined before passing it to dynamic_pricing
+        daily_rate = fetch_price_for_date(property_uid, day.strftime('%Y-%m-%d'))
         if daily_rate == 0:
             base_rate = fetch_base_rate(property_uid)
             if base_rate is None:
                 return "Failed to fetch base rate for the property."
             daily_rate = base_rate
-
+        # Now daily_rate is guaranteed to have a value
+        daily_rate = dynamic_pricing(daily_rate, pricing_rules, day, num_nights)
         # Apply dynamic pricing logic
         daily_rate = dynamic_pricing(daily_rate, pricing_rules, day, num_nights)
 
+        # Apply weekend rate
+        daily_rate = apply_weekend_rate(daily_rate, day, weekend_increase_percent=20, min_weekend_nights=2)
+
+        # Apply last-minute discount
+        today = datetime.now()
+        daily_rate = apply_last_minute_discount(daily_rate, day, today, [1, 3, 5], [30, 20, 10])
+
+        # Apply seasonal rate
+        seasonal_rates = [
+            {'start': datetime(today.year, 12, 31), 'end': datetime(today.year, 1, 2), 'percent': 20, 'min_nights': 2}
+        ]
+        daily_rate = apply_seasonal_rate(daily_rate, day, seasonal_rates)
+
         total_price += daily_rate
+
+        total_price = math.ceil(total_price)
 
     return f"The total price for the property from {from_date} to {to_date} is: {total_price}"
 
